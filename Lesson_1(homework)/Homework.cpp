07721ea43828548b77cb313c1590b415d4dd2a85
villsun <lonukkola@gmail.com>
2024-01-29 20:49:41 +0100

    Add function Fill_Random_Number

--- Lesson_1(homework)/Homework.cpp
+++ Lesson_1(homework)/Homework.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iostream>
 
 using namespace std;
 
@@ -6,6 +7,14 @@ int* Create_Array(int size)
 {
 	int* p_temp = new int[size];
 	return p_temp;
+}
+
+void Fill_Random_Number(int* p, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		p[i] = rand() % 11;
+	}
 }
 
 void Delete_Array(int* p)
@@ -16,7 +25,7 @@ void Delete_Array(int* p)
 
 int main()
 {
-
+	srand(static_cast<unsigned>(time(nullptr)));
 
 	return 0;
 }
